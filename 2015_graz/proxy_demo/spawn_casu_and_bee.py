48015b326be9beb028ebd9754d76af489912702d
Rob Mills <rob.mills@fc.ul.pt>
2015-04-21 10:37:54 +0100

    Initial version of a visual test for CASUs detecting bees
    - this version is for a single bee and a single casu, in simulation;
      and not using the deployment tool just yet.

---
+++ 2015_graz/proxy_demo/spawn_casu_and_bee.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+A simple simulation environment with one Casu and one bee.
+"""
+
+from assisipy import sim
+
+from math import pi
+
+if __name__ == '__main__':
+
+    simctrl = sim.Control()
+    
+    # Spawn the Bee and the Casu
+    simctrl.spawn('Casu','casu-001',(0,0,0))
+    simctrl.spawn('Bee','Bee-001',(2,0,pi/2))
